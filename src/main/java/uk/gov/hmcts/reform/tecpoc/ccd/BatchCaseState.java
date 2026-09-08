33d73b3a9f07fc4cbcb4dda294b9ffa22d3b64e3
Chris Neale <8654608+gonogo@users.noreply.github.com>
2026-09-08 16:35:35 +0100

    Label case headers as Case number / Batch number instead of #.

--- src/main/java/uk/gov/hmcts/reform/tecpoc/ccd/BatchCaseState.java
+++ src/main/java/uk/gov/hmcts/reform/tecpoc/ccd/BatchCaseState.java
@@ -4,12 +4,12 @@ import uk.gov.hmcts.ccd.sdk.api.CCD;
 
 public enum BatchCaseState {
 
-    @CCD(label = "Queued for processing")
+    @CCD(label = "Queued for processing", hint = "### Batch number: ${[CASE_REFERENCE]}")
     QUEUED_FOR_PROCESSING,
 
-    @CCD(label = "Processing started")
+    @CCD(label = "Processing started", hint = "### Batch number: ${[CASE_REFERENCE]}")
     PROCESSING_STARTED,
 
-    @CCD(label = "Processing complete")
+    @CCD(label = "Processing complete", hint = "### Batch number: ${[CASE_REFERENCE]}")
     PROCESSING_COMPLETE
 }
--- src/main/java/uk/gov/hmcts/reform/tecpoc/ccd/CaseState.java
+++ src/main/java/uk/gov/hmcts/reform/tecpoc/ccd/CaseState.java
@@ -4,15 +4,15 @@ import uk.gov.hmcts.ccd.sdk.api.CCD;
 
 public enum CaseState {
 
-    @CCD(label = "Pending Case Issued")
+    @CCD(label = "Pending Case Issued", hint = "### Case number: ${[CASE_REFERENCE]}")
     PENDING_CASE_ISSUED,
 
-    @CCD(label = "Case Issued")
+    @CCD(label = "Case Issued", hint = "### Case number: ${[CASE_REFERENCE]}")
     CASE_ISSUED,
 
-    @CCD(label = "Awaiting Respondent Response")
+    @CCD(label = "Awaiting Respondent Response", hint = "### Case number: ${[CASE_REFERENCE]}")
     AWAITING_RESPONDENT_RESPONSE,
 
-    @CCD(label = "Closed")
+    @CCD(label = "Closed", hint = "### Case number: ${[CASE_REFERENCE]}")
     CLOSED
 }
